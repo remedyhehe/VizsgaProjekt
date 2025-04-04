83b56c4d5830556375e291be69763c371ef439f9
Tonrok <petivagyok416@gmail.com>
2025-04-04 10:29:47 +0200

    Desktop App
    Igazából semmi érdemi, csak létrehoztam, a hétvégén nem nagyon lesz időm csinálni, szóval ez hétfő/kedden fog készülni

---
+++ Project/Collabears_Desktop/collabears/collabears/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace collabears
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Logined {count} time";
+            else
+                CounterBtn.Text = $"Logined {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+
+}
